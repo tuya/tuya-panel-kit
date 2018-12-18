93b7aced682248e5ae4f2e0e0a49ff5f9f695d76
sound2gd <429354772@qq.com>
2018-12-18 14:08:39 +0800

    remove unused funcs

--- src/utils/__tests__/time.js
+++ src/utils/__tests__/time.js
@@ -59,33 +59,3 @@ describe('TimeUtils stringToSecond', () => {
     expect(result).toEqual(79890);
   });
 });
-
-describe('TimeUtils dateToTimer', () => {
-  // 注意，只能一模一样的格式调用
-  it('TimeUtils.dateToTimer("20110801") = 1312128000', () => {
-    const result = TimeUtils.dateToTimer('20110801');
-    expect(result).toEqual(1312128000);
-  });
-
-  it('TimeUtils.dateToTimer("20110801 12:11:11") = 1312171871', () => {
-    const result = TimeUtils.dateToTimer('20110801 12:11:11');
-    expect(result).toEqual(1312171871);
-  });
-
-  it('TimeUtils.dateToTimer("22:11:30") = NaN', () => {
-    const result = TimeUtils.dateToTimer("22:11:30");
-    expect(result).toEqual(NaN);
-  });
-});
-
-describe('TimeUtils dateFormat', () => {
-  it('TimeUtils.dateFormat("yyyy-MM-dd", new Date()) = "2018-12-04"', () => {
-    const result = TimeUtils.dateFormat("yyyy-MM-dd", new Date(1543904283000));
-    expect(result).toEqual('2018-12-04');
-  });
-
-  it('TimeUtils.dateFormat("yyyy-MM-dd hh:mm:ss", new Date()) = 2018-03-16 17:49:47', () => {
-    const result = TimeUtils.dateFormat("yyyyyyyy-MM-dd hh:mm:ss", new Date(1521193787825));
-    expect(result).toEqual('2018-03-16 17:49:47');
-  });
-});
