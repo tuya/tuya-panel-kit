928e21f432a60020fafb38d14e4e8d297840b480
ShinyLeee <lshinylee007@gmail.com>
2019-04-03 15:34:09 +0800

    feat(TYListItem): 拓展应用范围
    新增actionDisabled props
    新增children props
    新增iconType props
    新增actionType props
    新增iconSize props
    新增iconColor props
    
    BREAKING CHANGE:
    Icon props 新增自动适配功能，若为string渲染IconFont组件，若为number或uri对象则渲染Image组件，其余向后兼容

--- src/components/TYLists/list-item.js
+++ src/components/TYLists/list-item.js
@@ -1,61 +1,132 @@
+/* eslint-disable new-cap */
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import {
   View,
   Text,
+  Image,
   TouchableOpacity,
+  ColorPropType,
   StyleSheet,
   ViewPropTypes,
 } from 'react-native';
 import shallowCompare from 'react-addons-shallow-compare';
+import TYText from '../TYText';
 import IconFont from '../iconfont';
-import { RatioUtils } from '../../utils';
+import defaultSvg from '../iconfont/defaultSvg';
 
-const {
-  convertX: cx,
-  convertY: cy,
-} = RatioUtils;
-
-/* eslint-disable max-len, new-cap */
-const icons = {
-  arrow:
-    'M225.63428295 804.43206399l76.86470558 76.86470558 499.4418311-499.44183112-499.4418311-499.44183111-76.86470558 76.86470558 422.57712554 422.57712553-422.57712554 422.57712554z',
-};
+const DEFAULT_ICON_FONT_SIZE = 28;
+const DEFAULT_ICON_FONT_COLOR = '#8E8E93';
 
 export default class TYListItem extends Component {
   static propTypes = {
     ...TouchableOpacity.propTypes,
-    styles: PropTypes.shape({
-      container: ViewPropTypes.style,
-      content: ViewPropTypes.style,
-      contentLeft: ViewPropTypes.style,
-      contentCenter: ViewPropTypes.style,
-      contentRight: ViewPropTypes.style,
-      title: Text.propTypes.style,
-      subTitle: Text.propTypes.style,
-    }),
+    styles: PropTypes.oneOfType([
+      PropTypes.bool,
+      PropTypes.shape({
+        container: ViewPropTypes.style,
+        content: ViewPropTypes.style,
+        contentLeft: ViewPropTypes.style,
+        contentCenter: ViewPropTypes.style,
+        contentRight: ViewPropTypes.style,
+        title: Text.propTypes.style,
+        subTitle: Text.propTypes.style,
+      }),
+    ]),
+    /**
+     * 是否在右边栏显示 arrow 图标
+     */
     arrow: PropTypes.bool,
+    /**
+     * arrow 图标颜色
+     */
+    arrowColor: ColorPropType,
+    /**
+     * 是否禁用列表点击事件，注意: Action点击事件不被此影响
+     */
     disabled: PropTypes.bool,
+    /**
+     * 是否禁用 `Action` 点击事件
+     */
+    actionDisabled: PropTypes.bool,
+    /**
+     * 标题
+     */
     title: PropTypes.string,
+    /**
+     * 副标题
+     */
     subTitle: PropTypes.string,
+    /**
+     * 子元素
+     */
+    children: PropTypes.element,
+    /**
+     * 左侧 Icon 类型，默认为 `auto`，根据传递进来的 `Icon` 类型自动判断，
+     */
+    iconType: PropTypes.oneOf(['auto', 'image', 'iconfont', 'text']),
+    /**
+     * 右侧 Action 类型，默认为 `auto`，根据传递进来的 `Action` 类型自动判断，
+     */
+    actionType: PropTypes.oneOf(['auto', 'image', 'iconfont', 'text']),
+    /**
+     * 图标尺寸，默认为 null，
+     * 若为 Image 图片大小跟随原尺寸
+     * 若为 IconFont 则拥有默认尺寸 `36`
+     */
+    iconSize: PropTypes.number,
+    /**
+     * 图标颜色，默认为 null，
+     * 若为 Image 只有指定了 iconColor 才会给图片上色
+     * 若为 IconFont 则拥有默认颜色 `#8E8E93`
+     *
+     */
+    iconColor: ColorPropType,
+    /**
+     * 左侧 Icon 具体值，当类型为 `string`时，默认使用 `IconFont`，
+     */
     Icon: PropTypes.oneOfType([
       PropTypes.element,
       PropTypes.func,
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.bool,
+      PropTypes.object,
     ]),
+    /**
+     * 右侧 Action 具体值，当类型为 `string`时，默认使用 `TYText`
+     */
     Action: PropTypes.oneOfType([
       PropTypes.element,
       PropTypes.func,
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.bool,
+      PropTypes.object,
     ]),
+    /**
+     * 是否需要重新渲染
+     */
     needUpdate: PropTypes.bool,
+    /**
+     * 列表项点击回调
+     */
     onPress: PropTypes.func,
   };
 
   static defaultProps = {
     styles: {},
     arrow: false,
+    arrowColor: '#C8C8C8',
     disabled: false,
+    actionDisabled: false,
     title: null,
     subTitle: null,
+    children: null,
+    iconSize: null,
+    iconColor: null,
+    iconType: 'auto',
+    actionType: 'auto',
     Icon: null,
     Action: null,
     needUpdate: true,
@@ -72,6 +143,7 @@ export default class TYListItem extends Component {
       'contentRight',
       'title',
       'subTitle',
+      'valueText',
     ];
   }
 
@@ -90,15 +162,173 @@ export default class TYListItem extends Component {
     return [defaultStyle, style];
   }
 
+  renderIcon({ contentLeftStyle, valueTextStyle }) {
+    const { iconSize, iconType, iconColor, Icon } = this.props;
+    if (!Icon) return null;
+    let iconComp;
+    if (iconType !== 'auto') {
+      switch (iconType) {
+        case 'image':
+          iconComp = (
+            <Image
+              style={[
+                iconSize && { width: iconSize, height: iconSize },
+                iconColor && { tintColor: iconColor },
+              ]}
+              source={Icon}
+            />
+          );
+          break;
+        case 'iconfont':
+          iconComp = (
+            <IconFont
+              size={iconSize || DEFAULT_ICON_FONT_SIZE}
+              name={defaultSvg[Icon] ? Icon : undefined} // 若 name 为undefined，则会使用d
+              d={Icon}
+              color={iconColor || DEFAULT_ICON_FONT_COLOR}
+            />
+          );
+          break;
+        case 'text':
+        default:
+          iconComp = <TYText style={valueTextStyle}>{Icon}</TYText>;
+          break;
+      }
+      return <View style={contentLeftStyle}>{iconComp}</View>;
+    }
+    switch (typeof Icon) {
+      case 'function':
+        iconComp = Icon();
+        break;
+      case 'string':
+        iconComp = (
+          <IconFont
+            size={iconSize || DEFAULT_ICON_FONT_SIZE}
+            name={defaultSvg[Icon] ? Icon : undefined} // 若 name 为undefined，则会使用d
+            d={Icon}
+            color={iconColor || DEFAULT_ICON_FONT_COLOR}
+          />
+        );
+        break;
+      case 'number':
+        iconComp = (
+          <Image
+            style={[
+              iconSize && { width: iconSize, height: iconSize },
+              iconColor && { tintColor: iconColor },
+            ]}
+            source={Icon}
+          />
+        );
+        break;
+      case 'object': {
+        if (Icon && Icon.uri) {
+          iconComp = (
+            <Image
+              style={[
+                iconSize && { width: iconSize, height: iconSize },
+                iconColor && { tintColor: iconColor },
+              ]}
+              source={Icon}
+            />
+          );
+        } else {
+          iconComp = Icon;
+        }
+        break;
+      }
+      default:
+        iconComp = Icon;
+        break;
+    }
+    return <View style={contentLeftStyle}>{iconComp}</View>;
+  }
+
+  renderAction({ valueTextStyle }) {
+    const { iconSize, actionType, iconColor, Action } = this.props;
+    if (!Action) return null;
+    let actionComp;
+    if (actionType !== 'auto') {
+      switch (actionType) {
+        case 'image':
+          actionComp = (
+            <Image
+              style={[
+                iconSize && { width: iconSize, height: iconSize },
+                iconColor && { tintColor: iconColor },
+              ]}
+              source={Action}
+            />
+          );
+          break;
+        case 'iconfont':
+          actionComp = (
+            <IconFont
+              size={iconSize || DEFAULT_ICON_FONT_SIZE}
+              name={defaultSvg[Action] ? Action : undefined}
+              d={Action}
+              color={iconColor || DEFAULT_ICON_FONT_COLOR}
+            />
+          );
+          break;
+        case 'text':
+        default:
+          actionComp = <TYText style={valueTextStyle}>{Action}</TYText>;
+          break;
+      }
+      return actionComp;
+    }
+    switch (typeof Action) {
+      case 'function':
+        actionComp = Action();
+        break;
+      case 'string':
+        actionComp = <TYText style={valueTextStyle}>{Action}</TYText>;
+        break;
+      case 'number':
+        actionComp = (
+          <Image
+            style={[
+              iconSize && { width: iconSize, height: iconSize },
+              iconColor && { tintColor: iconColor },
+            ]}
+            source={Action}
+          />
+        );
+        break;
+      case 'object': {
+        if (Action && Action.uri) {
+          actionComp = (
+            <Image
+              style={[
+                iconSize && { width: iconSize, height: iconSize },
+                iconColor && { tintColor: iconColor },
+              ]}
+              source={Action}
+            />
+          );
+        } else {
+          actionComp = Action;
+        }
+        break;
+      }
+      default:
+        actionComp = Action;
+        break;
+    }
+    return actionComp;
+  }
+
   render() {
     const {
       styles, // eslint-disable-line no-unused-vars
       arrow,
+      arrowColor,
       disabled,
-      Icon,
+      actionDisabled,
       title,
       subTitle,
-      Action,
+      children,
       onPress,
       onActionPress,
       ...touchProps
@@ -111,6 +341,7 @@ export default class TYListItem extends Component {
       contentRightStyle,
       titleStyle,
       subTitleStyle,
+      valueTextStyle,
     ] = this._styles.map(key => this.getStyle(key));
     return (
       <TouchableOpacity
@@ -121,9 +352,7 @@ export default class TYListItem extends Component {
         {...touchProps}
       >
         <View style={contentStyle}>
-          {Icon && (
-            <View style={contentLeftStyle}>{typeof Icon === 'function' ? Icon() : Icon}</View>
-          )}
+          {this.renderIcon({ contentLeftStyle, valueTextStyle })}
           <View style={contentCenterStyle}>
             {!!title && (
               <Text numberOfLines={1} style={titleStyle}>
@@ -131,14 +360,16 @@ export default class TYListItem extends Component {
               </Text>
             )}
             {!!subTitle && <Text style={subTitleStyle}>{subTitle}</Text>}
+            {children}
           </View>
           <TouchableOpacity
             activeOpacity={0.8}
             style={contentRightStyle}
+            disabled={actionDisabled}
             onPress={onActionPress || onPress}
           >
-            {typeof Action === 'function' ? Action() : Action}
-            {arrow && <IconFont d={icons.arrow} color="#C5CDD3" />}
+            {this.renderAction({ valueTextStyle })}
+            {arrow && <IconFont style={{ marginLeft: 6 }} name="arrow" color={arrowColor} />}
           </TouchableOpacity>
         </View>
       </TouchableOpacity>
@@ -150,19 +381,20 @@ export default class TYListItem extends Component {
 const defaultStyles = StyleSheet.create({
   container: {
     alignSelf: 'stretch',
-    minHeight: cy(48),
+    minHeight: 48,
     backgroundColor: '#fff',
   },
 
   content: {
     flex: 1,
+    alignItems: 'center',
     flexDirection: 'row',
-    paddingHorizontal: cx(16),
+    paddingHorizontal: 16,
   },
 
   contentLeft: {
     justifyContent: 'center',
-    marginRight: cx(12),
+    marginRight: 10,
   },
 
   contentCenter: {
@@ -173,15 +405,21 @@ const defaultStyles = StyleSheet.create({
   contentRight: {
     flexDirection: 'row',
     alignItems: 'center',
+    marginLeft: 10,
   },
 
   title: {
-    fontSize: cx(14),
+    fontSize: 14,
     color: '#22242C',
   },
 
   subTitle: {
-    fontSize: cx(12),
+    fontSize: 12,
     color: '#A2A3AA',
+  },
+
+  valueText: {
+    fontSize: 14,
+    color: '#999',
   },
 });
