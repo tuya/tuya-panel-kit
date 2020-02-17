cf65d3b2445cbf163c78ffbe8c3ee181b6ea4346
zengzhao <zengzhao@tuya.com>
2020-02-17 18:35:16 +0800

    wip(config): config

--- example/src/config/theme.js
+++ example/src/config/theme.js
@@ -1,5 +1,6 @@
 export default {
   global: {
+    brand: '#f00',
     background: '#f8f8f8',
     // background: {
     // '0%': 'red',
@@ -23,4 +24,15 @@ export default {
   //   color: '#fff',
   //   background: 'transparent',
   // },
+  popup: {
+    type: 'dark',
+  },
+  switchButton: {
+    light: {
+      tintColor: '#e5e5e5', // 关闭情况下背景色，不开放
+      onTintColor: '#00f', // 开启情况下背景色
+      thumbTintColor: '#000', // 关闭情况下滑块背景色
+      onThumbTintColor: '#fff', // 开启情况下滑块背景色
+    },
+  },
 };
