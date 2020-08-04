9918c599b11aab1b342c9bfe809736b526ef2f6c
曾照 <zengzhao@tuya.com>
2020-08-04 16:50:01 +0800

    feat(SwtichButton): adjust size && fix size&theme conflict

--- src/components/switch-button/__tests__/switch-button.js
+++ src/components/switch-button/__tests__/switch-button.js
@@ -3,15 +3,25 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import SwitchButton from '../index';
 
-
 describe('SwitchButton Component', () => {
   it('basic render', () => {
+    const component = renderer.create(<SwitchButton active={true} />);
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('SwitchButton size&theme conflict', function() {
     const component = renderer.create(
       <SwitchButton
+        value={true}
         active={true}
+        size={{ height: 60 }}
+        theme={{ width: 120, thumbSize: 55 }}
+        onChange={data => {
+          console.log(data);
+        }}
       />
     );
-
     expect(component.toJSON()).toMatchSnapshot();
   });
 
@@ -22,7 +32,7 @@ describe('SwitchButton Component', () => {
       <SwitchButton
         value={true}
         active={true}
-        onChange={(data) => {
+        onChange={data => {
           console.log(data);
         }}
       />
@@ -31,5 +41,4 @@ describe('SwitchButton Component', () => {
     expect(component.root.instance.props.value).toBe(true);
     expect(component.toJSON()).toMatchSnapshot();
   });
-
 });
