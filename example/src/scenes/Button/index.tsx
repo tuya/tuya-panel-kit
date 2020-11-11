c2841167e912e77504726d5d7c9c40136fd36926
youngjuning <youngjuning@aliyun.com>
2020-11-11 11:49:48 +0800

    chore: remove local d.ts

---
+++ example/src/scenes/Button/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Button } from 'tuya-panel-kit';
+
+export default class index extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
