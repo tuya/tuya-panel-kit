26d68264a30c372f04e0b020b27db86b4186808d
youngjuning <youngjuning@aliyun.com>
2020-11-13 15:58:55 +0800

    chore: 修复 jest 运行报错

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-optional-chaining',
-  ],
-};
