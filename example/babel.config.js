6e9f27eef6caa402e71bde3f470ef4e9b0d9dc07
youngjuning <youngjuning@aliyun.com>
2020-11-13 18:38:39 +0800

    wip: 修复合并请求中的review

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', '@babel/preset-env'],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-optional-chaining',
     [
       'module-resolver',
       {
         root: ['./'],
         alias: {
-          'tuya-panel-kit': '../src/index',
+          'tuya-panel-kit': '../',
         },
       },
     ],
