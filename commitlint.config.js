1f3824675c6e31a9f97a01c6bcaa052542d168f4
ZengZhao <zengzhao@tuya.com>
2021-03-31 19:15:54 +0800

    chore: Check whether the commit contains Chinese

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,5 +6,17 @@ module.exports = {
     'scope-case': [1, 'always', 'pascal-case'],
     'subject-case': [0],
     'subject-full-stop': [0],
+    'checkout-language': [2, 'always'], // support custom
   },
+  plugins: [
+    {
+      rules: {
+        'checkout-language': ({ subject, footer, body }) => {
+          const chinese = new RegExp('[\\u4E00-\\u9FFF]+', 'g');
+          const isEnglish = !chinese.test(subject) && !chinese.test(footer) && !chinese.test(body);
+          return [isEnglish, `Your commit content must be in English`];
+        },
+      },
+    },
+  ],
 };
