fb4984ba094f038ff5e9adfb20fd4be9654cd99d
ZengZhao <zengzhao@tuya.com>
2021-05-12 14:37:24 +0800

    fix(Notifacation): fix text and Icon misalignment on Android, fixes #63

--- src/components/notification/styled.js
+++ src/components/notification/styled.js
@@ -1,4 +1,4 @@
-import { View, TouchableOpacity, Image } from 'react-native';
+import { View, TouchableOpacity, Image, Text } from 'react-native';
 import styled from 'styled-components/native';
 import { RatioUtils } from '../../utils';
 import TYText from '../TYText';
@@ -17,9 +17,16 @@ export const StyledNotificationContent = styled(View)`
   align-items: center;
   justify-content: space-between;
   margin: ${`${12}px 0px ${2}px 0px`};
-  padding: ${`10px ${cx(16)}px`};
-  min-height: 24;
   border-radius: 22;
+  background-color: ${props => props.background};
+`;
+
+export const StyledNotificationContainer = styled(View)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: ${`10px ${cx(16)}px`};
+  min-height: 10;
   background-color: ${props => props.background};
 `;
 
@@ -27,28 +34,18 @@ export const StyledTitle = styled(TYText)`
   min-width: ${cx(44)}px;
   max-width: ${viewWidth - 142}px;
   font-size: 14;
-  line-height: 22;
   color: ${props => props.color};
-  margin: ${props =>
-    props.backIconCenter ? `0px ${cx(30)}px 0px ${cx(32)}px` : `0px ${cx(54)}px 0px ${cx(32)}px`};
-`;
-
-export const StyledButton = styled(TouchableOpacity)`
-  width: ${cx(24)}px;
-  height: 24;
-  justify-content: center;
+  margin: ${`0px ${cx(30)}px 0px ${cx(32)}px`};
 `;
 
 export const StyledIconFont = styled(IconFont)`
   position: absolute;
-  top: ${cx(10)}px;
-  left: ${cx(16)}px;
+  top: ${cx(3)}px;
 `;
 
 export const StyledImage = styled(Image)`
   position: absolute;
-  top: ${cx(10)}px;
-  left: ${cx(16)}px;
+  top: ${cx(2)}px;
   height: ${cx(21)}px;
   width: ${cx(24)}px;
 `;
