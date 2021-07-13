33671b780064c17d6bb541aec98bd990d2e99f73
洛竹 <youngjuning@aliyun.com>
2021-07-13 20:42:55 +0800

    chore: remove alias

--- example/babel.config.js
+++ example/babel.config.js
@@ -8,11 +8,6 @@ module.exports = {
         root: ['./'],
         extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
         alias: {
-          'tuya-panel-kit/lib/components/iconfont/svg/defaultSvg':
-            '../packages/tuya-panel-kit/src/components/iconfont/svg/defaultSvg',
-          'tuya-panel-kit/lib/components/modal/portalOut':
-            '../packages/tuya-panel-kit/src/components/modal/portalOut',
-          'tuya-panel-kit': '../packages/tuya-panel-kit/src/index.js',
           '#components': './src/components',
           '#config': './src/config',
           '#i18n': './src/i18n',
