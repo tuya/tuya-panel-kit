036377505115e9a29e0331074e5a8d85c999ed39
洛竹 <youngjuning@aliyun.com>
2021-07-14 11:03:18 +0800

    chore: add DEV_MODE env for web dev

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,3 +1,15 @@
+var getRNAlias = () => {
+  return process.env.DEV_MODE === 'umi'
+    ? {}
+    : {
+        'tuya-panel-kit/lib/components/iconfont/svg/defaultSvg':
+          '../packages/tuya-panel-kit/src/components/iconfont/svg/defaultSvg',
+        'tuya-panel-kit/lib/components/modal/portalOut':
+          '../packages/tuya-panel-kit/src/components/modal/portalOut',
+        'tuya-panel-kit': '../packages/tuya-panel-kit/src/index.js',
+      };
+};
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
@@ -8,6 +20,7 @@ module.exports = {
         root: ['./'],
         extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
         alias: {
+          ...getRNAlias(),
           '#components': './src/components',
           '#config': './src/config',
           '#i18n': './src/i18n',
