9a7bab225b8ecb48547b3f85c443a02e72099dc7
Zeng Beauty <2497752603@qq.com>
2021-08-06 16:45:32 +0800

    docs(kit): fix example in android and ios (#97)

--- example/src/pages/presentation/linear-gradient/index.tsx
+++ example/src/pages/presentation/linear-gradient/index.tsx
@@ -1,62 +1,94 @@
 import React from 'react';
+import { View } from 'react-native';
 import { Rect } from 'react-native-svg';
-import { LinearGradient } from 'tuya-panel-kit';
+import { LinearGradient, TYSectionList } from 'tuya-panel-kit';
 
 import { ListView } from '#components';
 import Strings from '#i18n';
 
 export default () => {
-  return (
-    <ListView
-      contentCenter={true}
-      list={[
+  const sections = [
+    {
+      title: Strings.getLang('lineargradient_two'),
+      data: [
         {
-          title: Strings.getLang('lineargradient_two'),
-          content: (
-            <LinearGradient
-              gradientId="Gradient1"
+          key: 0,
+          childView: (
+            <View
               style={{
-                width: 300,
                 height: 200,
-                marginLeft: 24,
-              }}
-              x1="0%"
-              y1="0%"
-              x2="0%"
-              y2="100%"
-              stops={{
-                '0%': 'red',
-                '100%': 'yellow',
+                marginBottom: 20,
+                alignItems: 'center',
+                justifyContent: 'center',
               }}
             >
-              <Rect width={300} height={200} />
-            </LinearGradient>
+              <LinearGradient
+                gradientId="Gradient1"
+                style={{
+                  width: 300,
+                  height: 200,
+                  marginLeft: 20,
+                  marginVertical: 20,
+                }}
+                x1="0%"
+                y1="0%"
+                x2="0%"
+                y2="100%"
+                stops={{
+                  '0%': 'red',
+                  '100%': 'yellow',
+                }}
+              >
+                <Rect width={300} height={200} />
+              </LinearGradient>
+            </View>
           ),
         },
+      ],
+    },
+    {
+      title: Strings.getLang('lineargradient_obl'),
+      data: [
         {
-          title: Strings.getLang('lineargradient_obl'),
-          itemStyle: {
-            marginTop: 200,
-          },
-          content: (
-            <LinearGradient
-              gradientId="Gradient2"
-              style={{ width: 300, height: 200, marginLeft: 24 }}
-              x1="100%"
-              y1="0%"
-              x2="0%"
-              y2="100%"
-              stops={{
-                '0%': 'red',
-                '30%': 'blue',
-                '100%': 'yellow',
+          key: 0,
+          childView: (
+            <View
+              style={{
+                height: 200,
+
+                alignItems: 'center',
+                justifyContent: 'center',
               }}
             >
-              <Rect width={300} height={200} />
-            </LinearGradient>
+              <LinearGradient
+                gradientId="Gradient2"
+                style={{ width: 300, height: 200, marginLeft: 20, marginVertical: 20 }}
+                x1="100%"
+                y1="0%"
+                x2="0%"
+                y2="100%"
+                stops={{
+                  '0%': 'red',
+                  '30%': 'blue',
+                  '100%': 'yellow',
+                }}
+              >
+                <Rect width={300} height={200} />
+              </LinearGradient>
+            </View>
           ),
         },
-      ]}
-    />
+      ],
+    },
+  ];
+  return (
+    <View style={{ flex: 1, marginTop: 10 }}>
+      <TYSectionList
+        sections={sections}
+        contentContainerStyle={{ backgroundColor: '#fff' }}
+        separatorStyle={{ backgroundColor: '#fff' }}
+        renderItem={({ item }) => item.childView}
+      />
+    </View>
   );
 };
--- example/src/pages/presentation/radial-gradient/index.tsx
+++ example/src/pages/presentation/radial-gradient/index.tsx
@@ -1,77 +1,92 @@
 import React from 'react';
-import { RadialGradient } from 'tuya-panel-kit';
-
-import { ListView } from '#components';
+import { View } from 'react-native';
+import { RadialGradient, TYSectionList } from 'tuya-panel-kit';
 import Strings from '#i18n';
 
 export default () => {
+  const sections = [
+    {
+      title: Strings.getLang('radialgradient_y2b'),
+      data: [
+        {
+          key: 0,
+          childView: (
+            <View style={{ width: 300, height: 200, marginTop: 10 }}>
+              <RadialGradient
+                gradientId="Gradient1"
+                style={{ width: 300, height: 200, marginLeft: 10 }}
+                stops={[
+                  {
+                    offset: '0%',
+                    stopColor: '#ff0',
+                    stopOpacity: '1',
+                  },
+                  {
+                    offset: '100%',
+                    stopColor: '#00f',
+                    stopOpacity: '1',
+                  },
+                ]}
+                rx="50%"
+                ry="50%"
+                fx="50%"
+                fy="50%"
+                cx="50%"
+                cy="50%"
+              />
+            </View>
+          ),
+        },
+      ],
+    },
+    {
+      title: Strings.getLang('radialgradient_ryp'),
+      data: [
+        {
+          key: 0,
+          childView: (
+            <View style={{ width: 300, height: 200, marginTop: 10 }}>
+              <RadialGradient
+                gradientId="Gradient2"
+                style={{ width: 300, height: 200, marginLeft: 10 }}
+                stops={[
+                  {
+                    offset: '0%',
+                    stopColor: 'red',
+                    stopOpacity: '1',
+                  },
+                  {
+                    offset: '50%',
+                    stopColor: 'yellow',
+                    stopOpacity: '1',
+                  },
+                  {
+                    offset: '100%',
+                    stopColor: 'pink',
+                    stopOpacity: '1',
+                  },
+                ]}
+                rx="50%"
+                ry="50%"
+                fx="100%"
+                fy="50%"
+                cx="50%"
+                cy="50%"
+              />
+            </View>
+          ),
+        },
+      ],
+    },
+  ];
   return (
-    <ListView
-      contentCenter={true}
-      list={[
-        {
-          title: Strings.getLang('radialgradient_y2b'),
-          content: (
-            <RadialGradient
-              gradientId="Gradient1"
-              style={{ width: 300, height: 200, marginLeft: 10 }}
-              stops={[
-                {
-                  offset: '0%',
-                  stopColor: '#ff0',
-                  stopOpacity: '1',
-                },
-                {
-                  offset: '100%',
-                  stopColor: '#00f',
-                  stopOpacity: '1',
-                },
-              ]}
-              rx="50%"
-              ry="50%"
-              fx="50%"
-              fy="50%"
-              cx="50%"
-              cy="50%"
-            />
-          ),
-        },
-        {
-          title: Strings.getLang('radialgradient_ryp'),
-          itemStyle: {
-            marginTop: 200,
-          },
-          content: (
-            <RadialGradient
-              gradientId="Gradient2"
-              style={{ width: 300, height: 200, marginLeft: 10 }}
-              stops={[
-                {
-                  offset: '0%',
-                  stopColor: 'red',
-                  stopOpacity: '1',
-                },
-                {
-                  offset: '50%',
-                  stopColor: 'yellow',
-                  stopOpacity: '1',
-                },
-                {
-                  offset: '100%',
-                  stopColor: 'pink',
-                  stopOpacity: '1',
-                },
-              ]}
-              rx="50%"
-              ry="50%"
-              fx="100%"
-              fy="50%"
-              cx="50%"
-              cy="50%"
-            />
-          ),
-        },
-      ]}
-    />
+    <View style={{ flex: 1, marginTop: 10 }}>
+      <TYSectionList
+        sections={sections}
+        contentContainerStyle={{ backgroundColor: '#fff' }}
+        separatorStyle={{ backgroundColor: '#fff' }}
+        renderItem={({ item }) => item.childView}
+      />
+    </View>
   );
 };
