3de95f0bbb6f75af6401485f385b1822f2d1d7ed
muhai <muhai.li@tuya.com>
2021-09-02 16:03:11 +0800

    fix: styles

--- example/tuya-panel-kit/src/pages/presentation/carousel/index.tsx
+++ example/tuya-panel-kit/src/pages/presentation/carousel/index.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Carousel, TYText } from 'tuya-panel-kit';
+import { Carousel, TYText, Utils } from 'tuya-panel-kit';
+
+const { convertX: cx } = Utils.RatioUtils;
 
 export default () => {
+  const Content = ({ backgroundColor, text, textBgColor }) => {
+    return (
+      <View
+        style={{
+          height: 200,
+          backgroundColor,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <View
+          style={{
+            width: cx(70),
+            height: cx(38),
+            backgroundColor: textBgColor,
+            borderRadius: cx(19),
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <TYText style={{ color: '#fff' }}>{text}</TYText>
+        </View>
+      </View>
+    );
+  };
   return (
     <Carousel
       style={{ height: 200 }}
@@ -10,16 +39,13 @@ export default () => {
       autoplay={true}
       loop={true}
       carouselChange={index => console.log(index)}
+      dotStyle={{
+        backgroundColor: '#F9895C',
+      }}
     >
-      <View style={{ height: 200, backgroundColor: 'red' }}>
-        <TYText style={{ color: '#333' }}>Carousel 1</TYText>
-      </View>
-      <View style={{ height: 200, backgroundColor: 'blue' }}>
-        <TYText style={{ color: '#333' }}>Carousel 2</TYText>
-      </View>
-      <View style={{ height: 200, backgroundColor: 'yellow' }}>
-        <TYText style={{ color: '#333' }}>Carousel 3</TYText>
-      </View>
+      <Content text="1" backgroundColor="#F82B00" textBgColor="#F85B0F" />
+      <Content text="2" backgroundColor="#F86016" textBgColor="#F97E4C" />
+      <Content text="3" backgroundColor="#F82B00" textBgColor="#F85B0F" />
     </Carousel>
   );
 };
