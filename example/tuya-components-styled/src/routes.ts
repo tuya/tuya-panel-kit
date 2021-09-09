146bcba352a1b4dcb986fab188d322f8b7ecc3be
ZengBeauty <2497752603@qq.com>
2021-09-09 19:25:27 +0800

    chore(style-demo): delete unuseful demo

--- example/tuya-components-styled/src/routes.ts
+++ example/tuya-components-styled/src/routes.ts
@@ -1,7 +1,4 @@
 import Strings from '#i18n';
-
-import Button from './pages/basic/button';
-import ButtonCard from './pages/basic/button-card';
 import SwitchButton from './pages/bool/switch-card';
 import ItemCard from './pages/show/item-card';
 import ArrowCard from './pages/number/arrow-card';
@@ -14,16 +11,6 @@ import ListCard from './pages/enum/list-card';
 
 // 这里的路由href要和文档markdown的demo路径一致
 export const routes = [
-  {
-    name: Strings.getLang('button'),
-    href: '/basic/button',
-    component: Button,
-  },
-  {
-    name: Strings.getLang('button_card'),
-    href: '/basic/button-card',
-    component: ButtonCard,
-  },
   // bool
   {
     name: Strings.getLang('switch_card'),
