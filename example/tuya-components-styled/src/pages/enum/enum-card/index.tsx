2a9d516d913adae50742c103789aaf14178e8100
Chechengyi <30520052+Chechengyi@users.noreply.github.com>
2021-09-30 14:12:19 +0800

    chore: demo Chinese (#147)

--- example/tuya-components-styled/src/pages/enum/enum-card/index.tsx
+++ example/tuya-components-styled/src/pages/enum/enum-card/index.tsx
@@ -11,33 +11,33 @@ const IMAGE =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==';
 const list = [
   {
-    label: '状态1',
+    label: 'status1',
     icon: TuyaRNSvgs.power,
     key: '0',
     disabled: true,
   },
   {
-    label: '状态2状态2状态2',
+    label: 'status2',
     icon: TuyaRNSvgs.power,
     key: '1',
   },
   {
-    label: '状态3',
+    label: 'status3',
     icon: TuyaRNSvgs.power,
     key: '2',
   },
   {
-    label: '状态4',
+    label: 'status4',
     icon: TuyaRNSvgs.power,
     key: '3',
   },
   {
-    label: '状态5',
+    label: 'status5',
     icon: TuyaRNSvgs.power,
     key: '4',
   },
   {
-    label: '状态6',
+    label: 'status6',
     icon: TuyaRNSvgs.power,
     key: '5',
   },
@@ -67,7 +67,7 @@ export default () => {
             <View>
               <ClassicEnumCard
                 width={290}
-                title="工作模式"
+                title="Title"
                 data={list}
                 // data={[]}
                 activeKey={activeKey1}
@@ -76,7 +76,7 @@ export default () => {
               <ClassicEnumCard
                 padding={[20, 20, 20, 20]}
                 style={{ marginTop: 20 }}
-                title="工作模式"
+                title="Title"
                 showTitle={false}
                 data={list.slice(0, 4)}
                 defaultActiveKey="1"
@@ -100,11 +100,11 @@ export default () => {
           title: Strings.getLang('nordic'),
           content: (
             <View>
-              <NordicEnumCard disabled title="工作模式" data={list} activeKey="0" />
+              <NordicEnumCard disabled title="Title" data={list} activeKey="0" />
               <NordicEnumCard
                 disabled
                 style={{ marginTop: 20 }}
-                title="传入图片"
+                title="Title"
                 showTitle={false}
                 data={list1.slice(0, 4)}
                 defaultActiveKey="1"
@@ -116,10 +116,10 @@ export default () => {
           title: Strings.getLang('acrylic'),
           content: (
             <View>
-              <AcrylicEnumCard title="工作模式" data={list} activeKey="0" />
+              <AcrylicEnumCard title="Title" data={list} activeKey="0" />
               <AcrylicEnumCard
                 style={{ marginTop: 20 }}
-                title="传入图片"
+                title="Title"
                 showTitle={false}
                 data={list1.slice(0, 4)}
                 defaultActiveKey="1"
