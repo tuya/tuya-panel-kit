eb5caae6906abd04328b1963c24f42a841f8cc66
ZengBeauty <2497752603@qq.com>
2021-10-28 19:04:30 +0800

    feat(SwitchButton): add tuya-panel-switch

---
+++ packages/tuya-panel-switch/src/interface.ts
@@ -0,0 +1,209 @@
+import { ViewStyle, StyleProp, TextStyle, Animated, ColorValue } from 'react-native';
+
+export interface IThemeSwitchProps extends ISwitchProps {
+  /**
+   * @description.zh 主题配置
+   * @description.en Theme configuration
+   * @default null
+   */
+  theme?: {
+    width?: number;
+    height?: number;
+    thumbSize?: number;
+    margin?: number | number[];
+    tintColor?:
+      | string
+      | {
+          [key: string]: string;
+        };
+    onTintColor?: string;
+    thumbTintColor?: string;
+    onThumbTintColor?: string;
+  };
+}
+
+export interface ISwitchProps {
+  /**
+   * @description.zh 容器样式
+   * @description.en Specify the style of the container that wraps the SwitchButton
+   * @types <a target='_blank' href='https://reactnative.dev/docs/view-style-props'>StyleProp<ViewStyle></a>
+   * @default undefined
+   */
+  style?: StyleProp<ViewStyle>;
+  /**
+   * @description.zh 测试标识
+   * @description.en Test identification
+   * @default "SwitchButton"
+   */
+  accessibilityLabel?: string;
+  /**
+   * @description.zh 是否禁用
+   * @description.en Whether to disable the SwitchButton.
+   * @default false
+   */
+  disabled?: boolean;
+  /**
+   * @description.zh 当前选中的值，设置了该属性即为受控组件
+   * @description.en The currently selected value. After this property is set, it is a controlled component.
+   * @default undefined
+   */
+  value?: boolean;
+  /**
+   * @description.zh 默认选中的值
+   * @description.en The value selected by default
+   * @default true
+   */
+  default?: boolean;
+  /**
+   * @description.zh 设置 SwitchButton 的大小
+   * @description.en Set the size of the development component
+   * @default undefined
+   */
+  size?: {
+    width?: number;
+    height?: number;
+    activeSize?: number;
+    margin?: number;
+    borderRadius?: number;
+  };
+  /**
+   * @description.zh 改变 SwitchButton 值时执行此回调
+   * @description.en This callback is executed when the switch combination value is changed.
+   * @default undefined
+   */
+  onValueChange: (value: boolean) => void;
+  /**
+   * @description.zh 设置当 SwitchButton的 value 为 false 时背景颜色
+   * @description.en Set the background color when the value of the SwitchButton is false.
+   * @default '#e5e5e5'
+   */
+  tintColor?: ColorValue;
+  /**
+   * @description.zh 设置当 SwitchButton的 value 为 true 时颜色
+   * @description.en Set the color when the value of the SwitchButton is true.
+   * @default '#44DB5E'
+   */
+  onTintColor?: ColorValue;
+  /**
+   * @description.zh 设置当 SwitchButton 的 value 为 false 时 thumb 颜色
+   * @description.en Set the color of the sliding button when the value of the SwitchButton is false.
+   * @default "#fff"
+   */
+  thumbTintColor?: string;
+  /**
+   * @description.zh 设置当 SwitchButton 的 value 为 true 时 thumb 颜色，若没有设置则为 thumbTintColor 的值
+   * @description.en Set the color of the sliding button when the value of the SwitchButton is true. It is the value of thumbTintColor if it is not set.
+   * @default undefined
+   */
+  onThumbTintColor?: string;
+  /**
+   * @description.zh 设置当 SwitchButton 的 value 为 false 时边框颜色 当 SwitchButton 的 value 为 true 时边框颜色等于 onTintColor
+   * @description.en Set the color of the border when the value of the sliding button is false.
+   * @default "#e5e5e5"
+   */
+  borderColor?: string;
+  /**
+   * @description.zh 指定 thumb 的样式
+   * @description.en Specify the style of the icon used for dragging in the switch.
+   * @types <a target='_blank' href='https://reactnative.dev/docs/view-style-props'>StyleProp<ViewStyle></a>
+   * @default null
+   */
+  thumbStyle?: StyleProp<ViewStyle>;
+  /**
+   * @description.zh 是否使用 Native Driver
+   * @description.en Whether to use Native Driver.
+   * @default true
+   */
+  useNativeDriver?: boolean;
+  /**
+   * @description.zh SwitchButton 的 value 值为 false 时左侧显示的字符，超过 3 个字符则显示显示 2 个字符，其余显示…
+   * @description.en When the value of SwitchButton is false, the characters displayed on the left side are displayed. If the value exceeds 3 characters, 2 characters are displayed, and the rest are displayed ...
+   * @default "ON"
+   */
+  onText?: string;
+  /**
+   * @description.zh SwitchButton 的 value 值为 true 时右侧显示的字符，超过 3 个字符则显示显示 2 个字符，其余显示…
+   * @description.en When the value of SwitchButton is true, the characters displayed on the right side are displayed. If the value exceeds 3 characters, 2 characters are displayed, and the rest are displayed ...
+   * @default "OFF"
+   */
+  offText?: string;
+  /**
+   * @description.zh SwitchButton 的 value 值为 false 时左侧显示的字符样式
+   * @description.en The character style displayed on the left when the value of SwitchButton is false
+   * @types <a target="_blank" href="https://reactnative.dev/docs/text-style-props">StyleProp<TextStyle></a>
+   * @default null
+   */
+  onTextStyle?: StyleProp<TextStyle>;
+  /**
+   * @description.zh SwitchButton 的 value 值为 true 时右侧显示的字符样式
+   * @description.en The character style displayed on the right when the value of SwitchButton is true
+   * @types <a target="_blank" href="https://reactnative.dev/docs/text-style-props">StyleProp<TextStyle></a>
+   * @default null
+   */
+  offTextStyle?: StyleProp<TextStyle>;
+  /**
+   * @description.zh Switch 开关类型，默认不传，特殊情况传值 thumbMore
+   * @description.en Switch Switch type, not transmitted by default, in special cases transmitted by thumbMore
+   * @default null
+   */
+  switchType?: 'thumbMore' | string;
+  /**
+   * @description.zh 当 switchType = 'thumbMore' 时，开关左右定位的滑块样式
+   * @description.en When switchType = 'thumbMore', switch the left and right positioning slider style
+   * @default null
+   */
+  smallThumbStyle?: StyleProp<ViewStyle>;
+  /**
+   * @description.zh 滑块上的图标路径
+   * @description.en Icon path on the slider
+   * @default null
+   */
+  d?: string;
+  /**
+   * @description.zh 滑块上的图标大小
+   * @description.en Size of the icon on the slider
+   * @default 18
+   */
+  iconSize?: number;
+  /**
+   * @description.zh 滑块上的图标颜色
+   * @description.en The icon color on the slider
+   * @default null
+   */
+  iconColor?: string;
+  /**
+   * @description.en Whether to support click vibration
+   * @description.zh 是否支持点按震动
+   * @default true
+   */
+  isVibration?: boolean;
+}
+
+export interface ISwitchState {
+  thumbLeft: Animated.Value;
+  leftThumbHeight: Animated.Value;
+  rightThumbHeight: Animated.Value;
+  height?: number;
+}
+
+export const switchDefaults = {
+  accessibilityLabel: 'SwitchButton',
+  defaultValue: true,
+  disabled: false,
+  onTintColor: '#44DB5E',
+  thumbTintColor: '#fff',
+  tintColor: '#e5e5e5',
+  borderColor: '#e5e5e5',
+  thumbStyle: null,
+  useNativeDriver: true,
+  onText: '',
+  offText: '',
+  onTextStyle: null,
+  offTextStyle: null,
+  d: null,
+  iconSize: 18,
+  iconColor: null,
+  switchType: null,
+  smallThumbStyle: null,
+  isVibration: true,
+};
---
+++ packages/tuya-panel-switch/src/switch.tsx
@@ -0,0 +1,302 @@
+import React from 'react';
+import {
+  Animated,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+  StyleProp,
+  NativeModules,
+} from 'react-native';
+import { Rect } from 'react-native-svg';
+import IconFont from 'tuya-panel-icon';
+import TYText from 'tuya-panel-text';
+import LinearGradient from 'tuya-panel-kit/lib/components/gradient/linear-gradient';
+import { ISwitchProps, switchDefaults, ISwitchState } from './interface';
+import { DEFAULT_SIZE, DEFAULT_GRADIENT_SIZE, EXTRA_WIDTH, EXTRA_HEIGHT } from './constants';
+
+export default class SwitchButton extends React.PureComponent<ISwitchProps, ISwitchState> {
+  static defaultProps = switchDefaults;
+
+  constructor(props) {
+    super(props);
+    const { value, defaultValue, smallThumbStyle } = props;
+    this.value = 'value' in props ? value : defaultValue;
+    let height;
+    if (smallThumbStyle && smallThumbStyle.height) {
+      [height] = smallThumbStyle;
+    }
+    const left = this.calcLeft(this.value);
+    this.state = {
+      height,
+      thumbLeft: new Animated.Value(left),
+      leftThumbHeight: new Animated.Value(this.value ? height || 16 : 0.2 * (height || 16)),
+      rightThumbHeight: new Animated.Value(this.value ? 0.2 * (height || 16) : height || 16),
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!('value' in nextProps) || this.value === nextProps.value) return;
+    this.valueChange(nextProps.value);
+  }
+
+  onSwitchChange = () => {
+    const { disabled, onValueChange, isVibration } = this.props;
+    if (disabled) return;
+    const newValue = !this.value;
+    if (!('value' in this.props)) {
+      this.valueChange(newValue);
+    }
+    if (NativeModules.TYRCTHapticsManager && isVibration) {
+      NativeModules.TYRCTHapticsManager.peek();
+    }
+    onValueChange && onValueChange(newValue);
+  };
+
+  get CGSize() {
+    if (!!this.props.onText && !!this.props.offText) {
+      return { ...DEFAULT_GRADIENT_SIZE, ...this.props.size };
+    }
+
+    return { ...DEFAULT_SIZE, ...this.props.size };
+  }
+
+  value: boolean;
+  _ref: any;
+  thumb: any;
+
+  calcLeft = value => {
+    const { width, activeSize, margin } = this.CGSize;
+    const left = value ? width - (activeSize + margin) : margin;
+    return left + EXTRA_WIDTH / 2;
+  };
+
+  calcColor = (value: boolean, type?: 'thumb' | 'border') => {
+    const { onThumbTintColor, thumbTintColor, onTintColor, tintColor, borderColor } = this.props;
+    if (type === 'thumb') {
+      const activeColor = onThumbTintColor || thumbTintColor;
+      return value ? activeColor : thumbTintColor;
+    }
+    if (type === 'border') {
+      return value ? onTintColor : borderColor;
+    }
+    return value ? onTintColor : tintColor;
+  };
+
+  valueChange = value => {
+    const { useNativeDriver, switchType } = this.props;
+    const { height } = this.state;
+    const color = this.calcColor(value);
+    const borderColor = this.calcColor(value, 'border');
+    const thumbColor = this.calcColor(value, 'thumb');
+    this._ref.setNativeProps({
+      style: { backgroundColor: color, borderColor },
+    });
+    this.thumb.setNativeProps({
+      style: { backgroundColor: thumbColor },
+    });
+    this.value = value;
+    const left = this.calcLeft(value);
+    this.state.thumbLeft.stopAnimation();
+    Animated.spring(this.state.thumbLeft, {
+      toValue: left,
+      // @ts-ignore
+      duration: 200,
+      useNativeDriver,
+    }).start();
+    if (switchType === 'thumbMore') {
+      Animated.parallel([
+        Animated.spring(this.state.leftThumbHeight, {
+          toValue: value ? height || 16 : 0.2 * (height || 16),
+          // @ts-ignore
+          duration: 200,
+          useNativeDriver: false,
+        }),
+        Animated.spring(this.state.rightThumbHeight, {
+          toValue: value ? 0.2 * (height || 16) : height || 16,
+          // @ts-ignore
+          duration: 200,
+          useNativeDriver: false,
+        }),
+      ]).start();
+    }
+  };
+
+  renderBackground() {
+    const { tintColor, onTintColor } = this.props;
+    const { borderRadius, width, height } = this.CGSize;
+    const backgroundColor = this.calcColor(this.value);
+    const borderColor = this.calcColor(this.value, 'border');
+    const wrapperStyle: StyleProp<ViewStyle> = {
+      width,
+      height,
+      borderRadius: borderRadius || (15.5 / 28) * height,
+      justifyContent: 'center',
+    };
+    const color = this.value ? onTintColor : tintColor;
+    if (typeof color === 'string') {
+      return (
+        <View
+          style={[
+            wrapperStyle,
+            {
+              backgroundColor,
+              borderColor,
+            },
+          ]}
+          ref={ref => {
+            this._ref = ref;
+          }}
+        />
+      );
+    }
+    if (typeof color === 'object') {
+      return (
+        <View
+          style={[
+            wrapperStyle,
+            {
+              backgroundColor: 'transparent',
+              borderColor: 'transparent',
+            },
+          ]}
+          ref={ref => {
+            this._ref = ref;
+          }}
+        >
+          <LinearGradient style={{ width, height }} stops={color} x1="0%" y1="0%" x2="100%" y2="0%">
+            <Rect x="0" y="0" width="100%" height="100%" rx={height / 2} />
+          </LinearGradient>
+        </View>
+      );
+    }
+    return null;
+  }
+
+  render() {
+    const { width, height, activeSize, margin } = this.CGSize;
+    const {
+      accessibilityLabel,
+      style,
+      disabled,
+      onText,
+      offText,
+      onTextStyle,
+      offTextStyle,
+      d,
+      iconSize,
+      iconColor,
+      switchType,
+    } = this.props;
+    const { leftThumbHeight, rightThumbHeight } = this.state;
+    const thumbColor = this.calcColor(this.value, 'thumb');
+    const containerStyle = [style, disabled && { opacity: 0.8 }];
+    const contentStyle: StyleProp<ViewStyle> = {
+      width: width + EXTRA_WIDTH,
+      height: Math.max(activeSize, height) + EXTRA_HEIGHT,
+      alignItems: 'center',
+      justifyContent: 'center',
+    };
+    const thumbStyle = [
+      {
+        width: activeSize,
+        height: activeSize,
+        borderRadius: activeSize / 2,
+        position: 'absolute',
+        transform: [{ translateX: this.state.thumbLeft }],
+        alignSelf: 'flex-start',
+        backgroundColor: thumbColor,
+        shadowColor: 'rgba(0, 0, 0, 0.3)',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowOpacity: 1,
+        shadowRadius: 3,
+        elevation: 2,
+      },
+      this.props.thumbStyle,
+    ];
+    const smallThumbStyle: StyleProp<ViewStyle> = [
+      {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 3,
+        position: 'absolute',
+        borderRadius: 1.5,
+        backgroundColor: '#FFF',
+      },
+      this.props.smallThumbStyle,
+    ];
+    const textOn = onText.length > 3 ? `${onText.substr(0, 2)}...` : onText;
+    const textOff = offText.length > 3 ? `${offText.substr(0, 2)}...` : offText;
+    return (
+      <View style={containerStyle} needsOffscreenAlphaCompositing>
+        <TouchableOpacity
+          style={contentStyle}
+          accessibilityLabel={accessibilityLabel}
+          activeOpacity={1}
+          onPress={this.onSwitchChange}
+        >
+          {this.renderBackground()}
+          {switchType === 'thumbMore' && (
+            <Animated.View
+              style={[smallThumbStyle, { left: 3 * margin, height: leftThumbHeight }]}
+            />
+          )}
+          {switchType === 'thumbMore' && (
+            <Animated.View
+              style={[
+                smallThumbStyle,
+                {
+                  right: 3 * margin,
+                  height: rightThumbHeight,
+                },
+              ]}
+            />
+          )}
+          {!!onText && (
+            <TYText
+              text={textOn}
+              style={[
+                {
+                  fontSize: 10,
+                  color: '#FFF',
+                  position: 'absolute',
+                  left: 6,
+                  fontWeight: '500',
+                  opacity: this.value ? 1 : 0,
+                },
+                onTextStyle,
+              ]}
+            />
+          )}
+          {!!offText && (
+            <TYText
+              text={textOff}
+              style={[
+                {
+                  fontSize: 10,
+                  color: '#999',
+                  position: 'absolute',
+                  right: 6,
+                  fontWeight: '500',
+                  opacity: this.value ? 0 : 1,
+                },
+                offTextStyle,
+              ]}
+            />
+          )}
+          <Animated.View
+            // @ts-ignore
+            style={[{ alignItems: 'center', justifyContent: 'center' }, thumbStyle]}
+            ref={ref => {
+              this.thumb = ref;
+            }}
+          >
+            {!!d && <IconFont d={d} size={iconSize} color={iconColor} />}
+          </Animated.View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
