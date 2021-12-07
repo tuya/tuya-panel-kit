1c3440bb87a3e1540f17e4890324b8caa87b32f0
Zeng Beauty <2497752603@qq.com>
2021-12-07 18:42:27 +0800

    feat(RadialGradient): add RadialGradient (#166)

---
+++ packages/tuya-panel-radial-gradient/jest.config.js
@@ -0,0 +1,20 @@
+const base = require('../../jest.config.base.js');
+const pkg = require('./package');
+
+module.exports = {
+  ...base,
+  name: pkg.name,
+  displayName: pkg.name,
+  rootDir: '../..',
+  testMatch: ['<rootDir>/packages/tuya-panel-radial-gradient/src/__tests__/?(*.)+(test).js'],
+  coverageDirectory: '<rootDir>/packages/tuya-panel-radial-gradient/coverage/',
+  collectCoverageFrom: ['<rootDir>/packages/tuya-panel-radial-gradient/src/*.tsx'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/example/node_modules',
+    '<rootDir>/example',
+    '<rootDir>/packages/*/lib',
+  ],
+  transform: {
+    '^.+\\.(t|j)sx?$': 'babel-jest',
+  },
+};
