c28514a25e3618039363601783d6b1da87732a97
Zeng Beauty <2497752603@qq.com>
2021-12-14 11:03:06 +0800

    feat(ble): The offline jump link was changed to the knowledge base H5 link (#167)
    Co-authored-by: ZengBeauty <zengzhao@tuya.com>

--- packages/tuya-panel-kit/src/components/layout/offline-view/index.js
+++ packages/tuya-panel-kit/src/components/layout/offline-view/index.js
@@ -24,6 +24,8 @@ const requireRnVersion = '5.23';
 
 const requireWifiRnVersion = '5.30';
 
+const jumpToRequireRnVersion = '5.54';
+
 export default class OfflineView extends Component {
   static propTypes = {
     style: ViewPropTypes.style,
@@ -101,9 +103,18 @@ export default class OfflineView extends Component {
     return isJumpToWifi && (role === 1 || role === 2);
   };
 
+  // 判断App RN版本是否为3.34.5及以上离线才能跳转知识库页面
+  _handleVersionJumpToKnowledge = () => {
+    const appRnVersion = get(TYNative, 'mobileInfo.appRnVersion');
+    const isGreater = appRnVersion && compareVersion(appRnVersion, jumpToRequireRnVersion);
+    const isAllowJumpTo = isGreater === 0 || isGreater === 1;
+    return isAllowJumpTo;
+  };
+
   _handleMoreHelp = () => {
     const { reconnectTextStyle } = this.props;
     const isJumpToWifi = this._handleVersionToJump();
+    const isAllowJumpTo = this._handleVersionJumpToKnowledge();
     let linkJumpStyle;
     if (isJumpToWifi) {
       linkJumpStyle = [
@@ -119,17 +130,36 @@ export default class OfflineView extends Component {
         color: '#999',
       };
     }
-    TYMobile.jumpSubPage(
-      { uiId: '000000cg8b' },
-      {
-        textLinkStyle: linkJumpStyle,
-      }
-    );
+
+    if (isAllowJumpTo) {
+      TYNative.getContentRouter()
+        .then(result => {
+          const { devId } = TYSdk.devInfo;
+          const { href } = result;
+          TYNative.jumpTo(`${href}?type=net_set&deviceId=${devId}`);
+        })
+        .catch(() => {
+          TYMobile.jumpSubPage(
+            { uiId: '000000cg8b' },
+            {
+              textLinkStyle: linkJumpStyle,
+            }
+          );
+        });
+    } else {
+      TYMobile.jumpSubPage(
+        { uiId: '000000cg8b' },
+        {
+          textLinkStyle: linkJumpStyle,
+        }
+      );
+    }
   };
 
   renderBleView() {
     const { deviceOnline, capability, isBleOfflineOverlay, renderBleOfflineView } = this.props;
     const isJumpToWifi = this._handleVersionToJump();
+    const isAllowJumpTo = this._handleVersionJumpToKnowledge();
     // 在蓝牙状态未获取到之前不渲染该页面
     if (typeof this.state.bluetoothStatus !== 'boolean') {
       return null;
@@ -144,6 +174,7 @@ export default class OfflineView extends Component {
         capability={capability}
         isBleOfflineOverlay={isBleOfflineOverlay}
         isJumpToWifi={isJumpToWifi}
+        isAllowJumpTo={isAllowJumpTo}
         onLinkPress={this._handleLinkPress}
       />
     );
@@ -161,6 +192,7 @@ export default class OfflineView extends Component {
     const isShowNewOffline = isGreater === 0 || isGreater === 1;
     const showOldOffline = get(TYSdk, 'devInfo.panelConfig.fun.showOldOffline', false);
     const isJumpToWifi = this._handleVersionToJump();
+    const isAllowJumpTo = this._handleVersionJumpToKnowledge();
     return !showOldOffline && isShowNewOffline ? (
       <NewOfflineView
         show={show}
@@ -170,6 +202,7 @@ export default class OfflineView extends Component {
         onHelpPress={this._handleMoreHelp}
         maskColor={maskColor}
         isJumpToWifi={isJumpToWifi}
+        isAllowJumpTo={isAllowJumpTo}
         reconnectTextStyle={reconnectTextStyle}
       />
     ) : (
